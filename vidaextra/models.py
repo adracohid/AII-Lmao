81cd3cfe4884c793c38c59ccd7275fff1819b81e
Adru18 <36424996+Adru18@users.noreply.github.com>
2019-01-22 19:03:26 +0100

    .

---
+++ vidaextra/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Noticia(models.Model):
+    titulo=models.CharField(max_length=200)
+    resumen=models.TextField()
+    link=models.CharField(max_length=500)
+    fecha=models.DateField()
+    hora=models.TimeField()
+    def __str__(self):
+        return self.titulo
+
+class Puntuacion(models.Model):
+    puntuacion=models.PositiveIntegerField()
+    userid=models.PositiveIntegerField()
+    noticiaid=models.PositiveIntegerField()
+
+
---
+++ vidaextra/scrapping.py
@@ -0,0 +1,41 @@
+from bs4 import BeautifulSoup
+import urllib.request,re
+
+def procesar_pagina(d:str):
+    fichero=urllib.request.urlopen(d)
+    documento=BeautifulSoup(fichero,"lxml")
+    return documento
+
+
+def extraer_titulo(e):
+    return e.h2.a.string
+def extraer_tipo(e):
+    return e.find(class_="abstract-taxonomy").string
+def extraer_resumen(e):
+    return e.find(class_="abstract-excerpt").p.string
+def extraer_link(e):
+    return e.find(class_="abstract-link-more")['href']
+
+def extraer_autor(e):
+    if(e.find(class_="abstract-author")==None):
+        return 'esta noticia no tiene autor'
+    else:
+        return e.find(class_="abstract-author").string
+def extraer_fecha(e):
+    return re.split('T',e.footer.time['titile'])[0]
+def extraer_hora(e):
+    return re.split('T',e.footer.time['titile'])[1][0:8]
+p=procesar_pagina("https://www.vidaextra.com/")
+
+l=p.find_all("div", class_=["abstract-content"])
+
+for e in l:
+    titulo=extraer_titulo(e)
+    resumen=extraer_resumen(e)
+    link=extraer_link(e)
+    comentarios=extraer_comentarios(e)
+    autor=extraer_autor(e)
+    fecha=extraer_fecha(e)
+    hora=extraer_hora(e)
+    print('Titulo: '+titulo +'\nResumen: '+resumen+ '\nLink: '+ link+ '\nComentarios: '+comentarios + '\nAutor: '+ autor+ '\nFecha:' 
+         + fecha + '\nHora: '+hora)
